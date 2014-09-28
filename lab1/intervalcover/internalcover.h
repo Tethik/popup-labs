d931e61069bd4298b3399c72064d9672c033a4c9
Joakim Uddholm <joakim@uddholm.com>
2014-09-28 10:49:39 +0200

    Internal Cover commented and authored.

--- lab1/intervalcover/internalcover.h
+++ lab1/intervalcover/internalcover.h
@@ -5,6 +5,13 @@
 
 #include <vector>
 
+/**
+ * AUTHORS: Joakim Uddholm, Per Classon
+ */
+
+/**
+ * Used to represent an interval. Has a left and a right position. Sortable using the < operator based on left position.
+ */
 struct Interval {
 	double left;
 	double right;
@@ -18,6 +25,9 @@ struct Interval {
 	}
 };
 
+/**
+ * Solves the interval cover problem given a goal interval and a list of intervals which are to be used to try and cover the goal.
+ */
 std::vector<int> cover(Interval& goal_interval, std::vector<Interval> intervals);
 
 #endif
